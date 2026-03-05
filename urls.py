e76699afe97c140993eba52e7029e91ef3b5d864
SarenFan <iicharimotodii@gmail.com>
2026-03-05 15:29:56 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,10 +3,7 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # Existing paths...
     path('<int:course_id>/', views.course_details, name='course_details'),
-    
-    # Add these two new paths for the exam feature
     path('<int:course_id>/submit/', views.submit, name='submit'),
     path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
